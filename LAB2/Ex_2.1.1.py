1514b232ed7069272effc1fd65bf055916e2df09
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-18 21:07:22 +0100

    Create Ex_2.1.1.py

---
+++ LAB2/Ex_2.1.1.py
@@ -0,0 +1,41 @@
+def main(number1, number2):
+    # a) Sum of the two numbers
+    total_sum = number1 + number2
+    print(f"{number1} + {number2} = {total_sum}")
+    # b) Difference of the two numbers
+
+    if (number1 > number2):
+        difference = number1 - number2
+        print(f"{number1} - {number2} = {difference}")
+    else:
+        difference = number2 - number1
+        print(f"{number1} - {number2} = {difference}")
+
+    # c) Multiplication of the two numbers
+
+    product = number1 * number2
+    print(f"{number1} x {number2} = {product}")
+
+    # d) Average value of the two numbers
+
+    average_value = (number1 + number2) / 2
+    print(f"({number1} + {number2})/2 = {average_value}")
+
+    # e)The absolute value of the difference
+    absolute_distance = abs(number1 - number2)
+    print(f"|{number1} - {number2}| = {absolute_distance}")
+
+    # f)Maximum value
+    max_number = max(number1, number2)
+    print(f"Maximum value between {number1} and {number2} is {max_number}")
+
+    # g) Minimum value
+    min_number = min(number1, number2)
+    print(f"Minimum value between {number1} and {number2} is {min_number}")
+
+
+if __name__ == "__main__":
+    input_num1 = int(input("Please enter your first number: "))
+    input_num2 = int(input("Please enter your second number: "))
+
+    main(input_num1, input_num2)
