0e0fbec70f6c28710f2a518a5bdcab13d7e4697e
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-18 21:31:36 +0100

    Update Ex_2.1.2.py

--- LAB2/Ex_2.1.2.py
+++ LAB2/Ex_2.1.2.py
@@ -1,2 +1,2 @@
 
-(https://user-images.githubusercontent.com/74207166/202796773-be2a1c5d-9110-4097-8275-58af78279267.jpg)
+#(https://user-images.githubusercontent.com/74207166/202796773-be2a1c5d-9110-4097-8275-58af78279267.jpg)
