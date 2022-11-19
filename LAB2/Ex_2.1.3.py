b17f649b1dabe7a43c93ccaf6dfd71a8d2beb33b
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-19 15:45:38 +0100

    Update Ex_2.1.3.py

--- LAB2/Ex_2.1.3.py
+++ LAB2/Ex_2.1.3.py
@@ -1,6 +1,7 @@
-# 02.1.3 Digits: Write a program that stores in a constant a positive five-digit integer (therefore between 10000 and 99999), 
-#and displays the individual digits of which it is composed. For example, having the number 16384, the program must display, on separate lines: 1 6 3 8 4.
-
+"""
+02.1.3 Digits: Write a program that stores in a constant a positive five-digit integer (therefore between 10000 and 99999), 
+and displays the individual digits of which it is composed. For example, having the number 16384, the program must display, on separate lines: 1 6 3 8 4.
+"""
 
 def digits(number):
 
