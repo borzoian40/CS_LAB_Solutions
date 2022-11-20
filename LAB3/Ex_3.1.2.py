3aa8aa653a0a701f4dace6c664bbc3111cf38c5e
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-20 23:17:35 +0100

    Create Ex_3.1.2.py

---
+++ LAB3/Ex_3.1.2.py
@@ -0,0 +1,13 @@
+"""
+
+03.1.2 Identikit of the string. Write a program that reads a string and displays the appropriate 
+messages, after checking if: 
+I. it contains only letters;
+II. it contains only capital letters;
+III. it contains only lowercase letters;
+IV. it contains only digits;
+V. it contains only letters and digits;
+VI. it starts with a lowercase letter;
+VII. it ends with a point.
+
+"""
