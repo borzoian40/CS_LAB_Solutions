069bcaf9da81b27410066573bc780945f5d587ac
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-21 20:22:11 +0100

    Update Ex_2.2.5.py

--- LAB2/Ex_2.2.5.py
+++ LAB2/Ex_2.2.5.py
@@ -4,18 +4,16 @@ and a sequence of numbers. Write a program that, starting from two serial codes,
 the screen in ascending order based on the numerical part only. Tip: Use the default functions of
 the Python language.
 """
-
-def main():
-
-    student_id_a = "S50123"
-    student_id_b = "S41235"
-
-    if int(student_id_a[1:])> int(student_id_b[1:]):
-        print(student_id_a)
-        print(student_id_b)
-    else: 
-        print(student_id_b)
-        print(student_id_a)
+    if int(student_id_1[1:])> int(student_id_2[1:]):
+        print(student_id_2)
+        print(student_id_1)
+    else:
+        print(student_id_1)
+        print(student_id_2)
 
 if __name__ == "__main__":
-    main()
+
+    print("Welcome!! Please enter two student id's.")
+    student_id1 = input("Student 1: ")
+    student_id2 = input("Student 2: ")
+    main(student_id1, student_id2)
