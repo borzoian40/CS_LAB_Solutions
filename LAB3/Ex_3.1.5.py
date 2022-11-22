ff6b9f4160c002091be1d660386ec1aa5233d095
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-22 17:14:01 +0100

    Update Ex_3.1.5.py

--- LAB3/Ex_3.1.5.py
+++ LAB3/Ex_3.1.5.py
@@ -1,2 +1,45 @@
 """
+03.1.5 Matter of logic. Write a program that takes as input an integer number x and displays the 
+following expressions on the screen, along with their truth values. Test the program with numerous 
+values of x.
+I. x > 0 and x < 100
+II. x > 0 or x < 100
+III. x > 0 or 100 < x
+IV. x > 0 and x < 100 or x == -1
+
 """
+def main(number):
+    integer_number = int(number)
+    i_value = True
+
+    #Condition 1
+    if (integer_number > 0 and integer_number < 100):
+        print(f"x > 0 and x < 100 : {i_value}")
+    else:
+        print(f"x > 0 and x < 100 : {not i_value}")
+
+    #Condition 2
+    if (integer_number > 0 or integer_number < 100):
+        print(f"x > 0 or x < 100: {i_value}")
+    else:
+        print(f"x > 0 or x < 100: {not i_value}")
+
+
+    #Condition 3
+    if (integer_number > 0 or 100 < integer_number):
+        print(f"x > 0 or 100 < x: {i_value}")
+    else:
+        print(f"x > 0 or 100 < x: {not i_value}")
+
+    #Condition 4
+    if (integer_number > 0 and integer_number < 100 or integer_number == -1):
+        print(f"x > 0 and x < 100 or x == -1: {i_value}")
+    else:
+        print(f"x > 0 and x < 100 or x == -1: {not i_value}")
+
+
+
+
+if __name__ == "__main__":
+    number = int(input("Please type your number: "))
+    main(number)
