12dc9f3ab2d953f41c1c7d78ea475fcba0f3b352
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-22 16:49:03 +0100

    Update Ex_3.1.4.py

--- LAB3/Ex_3.1.4.py
+++ LAB3/Ex_3.1.4.py
@@ -1,2 +1,21 @@
 """
+03.1.4 It’s equal. Describe the pseudocode corresponding to the following Python program:
+x = 3.0
+s == 'seven point five'
+if x = 3.0:
+ s == 'three point zero'
+print(s)
+What is the output of this program?
+""" 
+
 """
+The program contains THREE errors:
+1. On line 4 and line 6, when we want to assign a value to a variable, we need to insert only 1 "=" EQUAL sign.
+2. Line 5, there should be 2 EQUAL signs when comparing in conditional statements.
+"""
+x = 3.0
+s = 'seven point five'
+if x == 3.0:
+ s = 'three point zero'
+print(s)
+
