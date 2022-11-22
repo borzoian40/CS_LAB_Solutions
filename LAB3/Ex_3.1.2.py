c331f69ee08443b0353a917c0028d297684886bd
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-22 16:30:42 +0100

    Update Ex_3.1.2.py

--- LAB3/Ex_3.1.2.py
+++ LAB3/Ex_3.1.2.py
@@ -11,3 +11,23 @@ VI. it starts with a lowercase letter;
 VII. it ends with a point.
 
 """
+def main(input_message):
+
+    if input_message.isalpha():
+        print("The given input contains only letters.")
+    elif input_message.isdigit():
+        print("The given input contains only digits.")
+
+    if input_message.isalpha and input_message.isupper():
+        print("The given input contains only uppercase letters.")
+    elif input_message.isalpha and input_message.islower():
+        print("The given input contains only lowercase letters.")
+
+    if input_message.alnum():
+        print("The given input contains letters and digits only.")
+
+    if input_message[0].islower():
+        print("The given input starts with a lowercase letter.")
+
+    if input_message.endswith("."):
+        print("The given input is a sentence!")
