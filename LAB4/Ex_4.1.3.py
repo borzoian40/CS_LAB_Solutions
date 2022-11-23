dadef6cdb7d52d3eb01d42c7d8e05ec06d2bc81e
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-23 22:25:52 +0100

    Create Ex_4.1.3.py

---
+++ LAB4/Ex_4.1.3.py
@@ -0,0 +1,17 @@
+"""
+04.1.3 Shapes. Write a program that takes as an input an integer number n and prints a square and a 
+rhombus filled with asterisks (*), with each side long n asterisks. Example: using n=4, the program 
+shows: 
+****
+****
+****
+****
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+"""
