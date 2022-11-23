f22c1835af6d1316b80f49a75ba3ab81129fba96
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-23 22:13:50 +0100

    Update Ex_4.1.1.py

--- LAB4/Ex_4.1.1.py
+++ LAB4/Ex_4.1.1.py
@@ -21,27 +21,27 @@ number_str = int(input("How many numbers do you want in your list? "))
 sum = 0
 list_ofnumbers = [0] * number_str
 list_even = []
-list_odd_num = []
-
-max = list_ofnumbers[0]
-
+list_odd = []
 for index in range(number_str):
     number = int(input(f"{index + 1}. Type in your number: "))
     list_ofnumbers[index] = number
-    if(number%2==0):
-        list_even.append(number)
-    else:
-        list_odd_num.append(number)
 
-    min = list_ofnumbers[0]
-    sum += number
+max = list_ofnumbers[0]
+min = list_ofnumbers[0]
+for index in range(number_str):
+    if (list_ofnumbers[index] % 2 == 0):
+        list_even.append(list_ofnumbers[index])
+    else:
+        list_odd.append(list_ofnumbers[index])
+
+    sum += list_ofnumbers[index]
     if (list_ofnumbers[index] > max):
         max = list_ofnumbers[index]
     elif (list_ofnumbers[index] <= min):
         min = list_ofnumbers[index]
 
+    print(f"The sum value is: {sum - list_ofnumbers[index]} + {list_ofnumbers[index]} = {sum}")
+print("********************")
+print(f"The maximum value is: {max}\nThe minimum value is: {min}\nThe list of even numbers are: {sorted(list_even)}"
+      f"\nThe list of odd numbers are: {sorted(list_odd)}")
 
-    print(
-        f"The sum value is: {sum - number} + {number} = {sum}\nThe maximum value is: {max}.\nThe minimum value is: {min}")
-
-print(f"The list of even numbers are: {list_even}\nThe list of odd numbers are: {list_odd_num}")
