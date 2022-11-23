6713eee102f7d57fbe782a886ed30f85e0fb4401
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-23 22:25:13 +0100

    Create Ex_4.1.2.py

---
+++ LAB4/Ex_4.1.2.py
@@ -0,0 +1,10 @@
+"""
+04.1.2 String analysis. Write a program that takes as an input a line of text as a string and outputs 
+the following requests: 
+I. Only the capital letters in the string.
+II. The letters in the string in even positions.
+III. The same string with vowels (uppercase and lowercase) replaced by an underscore (_).
+IV. How many digits are contained in the string.
+V. The positions of all vowels in the string.
+
+"""
