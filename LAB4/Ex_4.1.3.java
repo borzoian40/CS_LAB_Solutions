ad32164d768a4e673cb42aeef8e8d27f7a827fa9
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-25 18:12:53 +0100

    Update Ex_4.1.3.java

--- LAB4/Ex_4.1.3.java
+++ LAB4/Ex_4.1.3.java
@@ -23,28 +23,28 @@ public class Ex_4.1.3{
     
     for(int i = 0; i<row; i++){
       for(int j = column; j >=i; j--){
-      System.out.print(" ")    
+      System.out.print(" ");   
       }
       for(int z = 0; z<=i; z++){
-        System.out.print("*")
+        System.out.print("*");
         }
       for(int k = 0; k<i; k++){
-        System.out.print("*")
+        System.out.print("*");
        }
-      System.out.println()
+      System.out.println();
     }//end of first outer loop
     
     for(int i = 0; i<row-1; i++){
       for(int j = i; j<column-1; j++){
-      System.out.print(" ")
+      System.out.print(" ");
       }
       for(int k = 0; k<row-2; k++){
-      System.out.print("*")
+      System.out.print("*");
       }
       for(int j = 0; j<column-2; k++){
-        System.out.print("*")
+        System.out.print("*");
        }
-    System.out.println()  
+    System.out.println();  
     }//end of second outer for loop
   
   
