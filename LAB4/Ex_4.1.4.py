b465ef764ec087b5d1c71f27a3a9112f3c8e0010
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-26 09:58:36 +0100

    Create Ex_4.1.4.py

---
+++ LAB4/Ex_4.1.4.py
@@ -0,0 +1,19 @@
+
+"""
+ Words in reverse. Write a program that reads a word and outputs: 
+I. The reversed word. If the user writes the string 'Hello', the program shall output
+'olleH':
+"""
+def main(string):
+    print(string[::-1])
+
+if __name__ == "__main__":
+    something = input("Please enter your name: ")
+    main(something)
+"""
+II. The uppercase letters starting from the end. If the user writes the string 'HeLlO', the 
+program shall output 'OLH'.
+"""
+def 
+
+
