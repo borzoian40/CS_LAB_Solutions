b87c5d0cd4432d266f6ae6f441b369265a09c6e5
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-27 22:00:19 +0100

    Create Ex_4.1.8.py

---
+++ LAB4/Ex_4.1.8.py
@@ -0,0 +1,8 @@
+"""
+04.1.8 Duplicate numbers. Write a program reading a sequence of integer numbers (the sequence 
+ends with an empty line) and, after each acquisition, compute and shows only the adjacent duplicate 
+numbers.
+All adjacent duplicates. For example, if the input is 1 3 3 4 5 5 6 6 6 2, the 
+program should print 3 5 6. 
+
+"""
