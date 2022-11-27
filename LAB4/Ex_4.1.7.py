a158238db0b76e258f7452fa13af8f23d2a9d4cf
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-27 19:06:50 +0100

    Create Ex_4.1.7.py

---
+++ LAB4/Ex_4.1.7.py
@@ -0,0 +1,11 @@
+"""
+04.1.7 Words and spaces. Write a program reading a word and showing all its substring, sorted by 
+increasing length. If the user inputs the string 'rum', the program shall output:
+r 
+u 
+m 
+ru 
+um 
+rum
+
+"""
