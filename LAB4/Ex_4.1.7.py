66cdd57d6509e01089b6cf0e4cd837f41915f369
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-27 21:22:51 +0100

    Update Ex_4.1.7.py

--- LAB4/Ex_4.1.7.py
+++ LAB4/Ex_4.1.7.py
@@ -9,3 +9,15 @@ um
 rum
 
 """
+
+def main(words):
+    for i in range(1, len(words) + 1):
+        print(words[i - 1])
+    for j in range(1, len(words)):
+        for k in range(1, len(words)-1):
+            print(words[k - 1:k + j:1])
+
+
+if __name__ == "__main__":
+    words = input("Please enter your word: ")
+    main(words)
