76199e9bb8ee9cda0cfdae5e55574e92c20f046c
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-27 12:27:43 +0100

    Create Ex_4.1.6.py

---
+++ LAB4/Ex_4.1.6.py
@@ -0,0 +1,13 @@
+"""
+04.1.6 Prime numbers. Write a program that asks the user for an integer number and shows all the 
+prime numbers lower or equal to that number. Example: if the user inputs 20, the program shall 
+output: 
+2 
+3 
+5 
+7 
+11 
+13 
+17 
+19 
+"""
