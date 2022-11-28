55938cfd71a98b1c05b70a00742cba77c9de5fda
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-28 20:41:15 +0100

    Create Ex_6.1.1.py

---
+++ LAB6/Ex_6.1.1.py
@@ -0,0 +1,7 @@
+"""
+06.1.1 Vowel Count. Write the function: 
+def count_vowels(string)
+that returns the number of vowels in the string. Vowels are the letters a, e, i, o, and u; as well as 
+their respective capitalized versions.
+
+"""
