411ced2e2d64f141231720e24d33a3b1a119d572
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-29 00:03:28 +0100

    Update Ex_6.1.3.py

--- LAB6/Ex_6.1.3.py
+++ LAB6/Ex_6.1.3.py
@@ -8,8 +8,8 @@ def cone_volume(r,h)
 def cone_surface(r,h)
 
 """
-import math
 
+import math
 
 def sphere_volume(r):
     volume_Sphere = float((4 / 3) * (math.pi) * (r ** 3))
@@ -19,13 +19,13 @@ def sphere_volume(r):
 
 def sphere_surface(r):
     surface_Sphere = float((4) * (math.pi) * (r ** 2))
-    print(f"{surface_Sphere:0.2f}")
+    print(f"Sphere surface: {surface_Sphere:0.2f}")
     # 4(pi)(r**2)
 
 
 def cylinder_volume(r, h):
     volume_Cylinder = float((math.pi) * (r ** 2) * (h))
-    print(f"{volume_Cylinder: 0.2f}")
+    print(f"Cylinder Volume:{volume_Cylinder: 0.2f}")
 
 
 # (pi)(r**2)(h)
@@ -34,14 +34,14 @@ def cylinder_surface(r, h):
     surface_Cylinder = float(
         (2 * (math.pi) * r * h) + (2 * (math.pi) * (r ** 2))
     )
-    print(f"{surface_Cylinder: 0.2f}")
+    print(f"Cylinder surface:{surface_Cylinder: 0.2f}")
 
 
 # 2πrh+2πr**2
 
 def cone_volume(r, h):
     volume_Cone = float((math.pi) * (r ** 2) * (h / 3))
-    print(f"{volume_Cone: 0.2f}")
+    print(f"Cone volume: {volume_Cone: 0.2f}")
 
 
 # (pi)(r**2)(h/3)
@@ -49,7 +49,9 @@ def cone_volume(r, h):
 def cone_surface(r, h):
     l = float((r ** 2) + (h ** 2))
     surface_Cone = float((math.pi) * (r ** 2) + (math.pi) * (r) * l)
-    print(f"{surface_Cone: 0.2f}")
+    print(f"Cone surface: {surface_Cone: 0.2f}")
+
+
 ##SA = πr2 + πrl
 
 if __name__ == "__main__":
@@ -57,6 +59,7 @@ if __name__ == "__main__":
     height = int(input("Please enter the length of your height: "))
     sphere_volume(radius)
     sphere_surface(radius)
-    cylinder_volume(radius,height)
-    cylinder_surface(radius,height)
-    
+    cylinder_volume(radius, height)
+    cylinder_surface(radius, height)
+    cone_volume(radius, height)
+    cone_surface(radius, height)
