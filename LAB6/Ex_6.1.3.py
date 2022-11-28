4dcf57cc3e2942942a40ce60a6ab5070c6c20b95
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-28 23:56:36 +0100

    Update Ex_6.1.3.py

--- LAB6/Ex_6.1.3.py
+++ LAB6/Ex_6.1.3.py
@@ -8,3 +8,47 @@ def cone_volume(r,h)
 def cone_surface(r,h)
 
 """
+import math
+
+
+def sphere_volume(r):
+    volume_Sphere = float((4 / 3) * (math.pi) * (r ** 3))
+    print(f"Sphere Volume: {volume_Sphere:0.2f}")
+    # 4/3(pi)(r**3)
+
+
+def sphere_surface(r):
+    surface_Sphere = float((4) * (math.pi) * (r ** 2))
+    print(f"{surface_Sphere:0.2f}")
+    # 4(pi)(r**2)
+
+
+def cylinder_volume(r, h):
+    volume_Cylinder = float((math.pi) * (r ** 2) * (h))
+    print(f"{volume_Cylinder: 0.2f}")
+
+
+# (pi)(r**2)(h)
+
+def cylinder_surface(r, h):
+    surface_Cylinder = float(
+        (2 * (math.pi) * r * h) + (2 * (math.pi) * (r ** 2))
+    )
+    print(f"{surface_Cylinder: 0.2f}")
+
+
+# 2πrh+2πr**2
+
+def cone_volume(r, h):
+    volume_Cone = float((math.pi) * (r ** 2) * (h / 3))
+    print(f"{volume_Cone: 0.2f}")
+
+
+# (pi)(r**2)(h/3)
+
+def cone_surface(r, h):
+    l = float((r ** 2) + (h ** 2))
+    surface_Cone = float((math.pi) * (r ** 2) + (math.pi) * (r) * l)
+    print(f"{surface_Cone: 0.2f}")
+##SA = πr2 + πrl
+
