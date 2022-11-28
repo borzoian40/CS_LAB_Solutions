b2a2d7c4369002a2bf6f17a64c361ad455d320eb
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-29 00:05:48 +0100

    Create Ex_6.1.4.py

---
+++ LAB6/Ex_6.1.4.py
@@ -0,0 +1,4 @@
+"""
+06.1.4 Bank Balance. Write a function that calculates the balance of a bank account by crediting interest annually. 
+The function receives as parameters: the number of years, the initial balance, and the annual interest rate. 
+"""
