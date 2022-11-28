26bd10b948daaa4d79baab1ef211a736e4611f30
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-28 21:30:13 +0100

    Update Ex_6.1.2.py

--- LAB6/Ex_6.1.2.py
+++ LAB6/Ex_6.1.2.py
@@ -4,4 +4,24 @@ Words are separated by spaces.
 For example, countWords("Mary had a little lamb") should return 5. 
 
 """
+#First approach using SPLIT
 
+def count_Words(input):
+    print(len(input.split()))
+
+if __name__ == "__main__":
+    words = input("Please enter your word: ")
+    count_Words(words)
+
+#Second approach by counting the spaces: " " between words. And in the end adding 1.
+def count_Words(string):
+    count = 0
+    for i in range(len(string)):
+        if (string[i] == " "):
+            count += 1
+    print(count+1)
+
+
+if __name__ == "__main__":
+    word = input("Please enter your word: ")
+    count_Words(word)
