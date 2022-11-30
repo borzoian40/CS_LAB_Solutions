38f57075ea80d24533d4aea4b1b5c8ee09cc122e
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-12-01 00:48:08 +0100

    Update Ex_7.1.2.py

--- LAB7/Ex_7.1.2.py
+++ LAB7/Ex_7.1.2.py
@@ -1,6 +1,5 @@
 """
-07.1.2 List of random numbers. Write a program that initializes a list with ten random integers 
-between 1 and 100 and then displays, on four successive lines: 
+07.1.2 List of random numbers. Write a program that initializes a list with ten random integers between 1 and 100 and then displays, on four successive lines: 
 I. All elements of even index; 
 II. All the elements that are even; 
 III. All elements in reverse order; 
