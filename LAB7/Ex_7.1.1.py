a8d8f91ce6132b156a880211f7754cd0f4c90c90
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-30 18:33:20 +0100

    Create Ex_7.1.1.py

---
+++ LAB7/Ex_7.1.1.py
@@ -0,0 +1,7 @@
+"""
+07.1.1 Sum with alternating signs. Write a program that receives as input a sequence of integers 
+(terminated by an empty line), and that calculates the alternating sum of its elements. For example, if 
+the program reads the data 1 4 9 16 9 7 4 9 11, it must calculate and display 
+1 - 4 + 9 - 16 + 9 - 7 + 4 - 9 + 11 = –2. 
+
+"""
