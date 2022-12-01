844e7dc595d7f37858a3ea0e8079fbe2184b9412
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-12-01 11:00:53 +0100

    Create Ex_7.1.4.py

---
+++ LAB7/Ex_7.1.4.py
@@ -0,0 +1,6 @@
+"""
+07.1.4 Local highs. Read a sequence of integers ended by a blank line. Print the position of the local 
+maxima (numbers greater than both the previous and the next value) if there are any, otherwise print 
+the message 'There are no local maxima'. 
+Extension: if there are several pairs of local maxima, identify the two closest local maxima and print their position.
+"""
