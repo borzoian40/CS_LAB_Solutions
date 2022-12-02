05ca22eb5db7d19f4a186ceb313e404d593fbe75
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-12-02 17:56:46 +0100

    Update Ex_7.1.4.py

--- LAB7/Ex_7.1.4.py
+++ LAB7/Ex_7.1.4.py
@@ -4,3 +4,23 @@ maxima (numbers greater than both the previous and the next value) if there are 
 the message 'There are no local maxima'. 
 Extension: if there are several pairs of local maxima, identify the two closest local maxima and print their position.
 """
+
+def find_Local_Maxima(length, array):
+    # An array to store the indices of the local maxima values.
+    maxima = []
+
+    if (array[0] > array[1]):
+        maxima.append(0)
+
+
+    for i in range(1, length - 1):
+        if (array[i - 1] < array[i] > array[i + 1]):
+            maxima.append(i)
+
+    if (array[-1] > array[-2]):
+        maxima.append(0)
+    if (len(maxima)>0):
+      print("Indices where the local maxima is located: ")
+      print(*maxima)
+    else:
+      print("There are no local maxima.")
