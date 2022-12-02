b1a1235c96a1594c2845b02b4857ee825eb3dd10
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-12-02 18:24:05 +0100

    Create Ex_7.1.5.py

---
+++ LAB7/Ex_7.1.5.py
@@ -0,0 +1,7 @@
+"""
+07.1.5. Write the same_set(a, b) function that checks if two lists contain the 
+same elements, regardless of the order and ignoring the presence of duplicates. 
+For example, the two lists 1 4 9 16 9 7 4 9 11 and 11 11 7 9 16 4 1 must be considered equal. 
+The function must not modify the lists that have been passed as parameters. 
+
+"""
