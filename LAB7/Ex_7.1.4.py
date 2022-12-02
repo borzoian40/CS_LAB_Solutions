88d32063f32bd3fc1a384b2ac54fa6684c4f6d2a
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-12-02 18:02:23 +0100

    Update Ex_7.1.4.py

--- LAB7/Ex_7.1.4.py
+++ LAB7/Ex_7.1.4.py
@@ -1,10 +1,3 @@
-"""
-07.1.4 Local highs. Read a sequence of integers ended by a blank line. Print the position of the local 
-maxima (numbers greater than both the previous and the next value) if there are any, otherwise print 
-the message 'There are no local maxima'. 
-Extension: if there are several pairs of local maxima, identify the two closest local maxima and print their position.
-"""
-
 def find_Local_Maxima(length, array):
     # An array to store the indices of the local maxima values.
     maxima = []
@@ -12,25 +5,24 @@ def find_Local_Maxima(length, array):
     if (array[0] > array[1]):
         maxima.append(0)
 
-
     for i in range(1, length - 1):
         if (array[i - 1] < array[i] > array[i + 1]):
             maxima.append(i)
 
     if (array[-1] > array[-2]):
         maxima.append(0)
-    if (len(maxima)>0):
-      print("Indices where the local maxima is located: ")
-      print(*maxima)
+    if (len(maxima) > 0):
+        print("Indices where the local maxima is located: ")
+        print(*maxima)
     else:
-      print("There are no local maxima.")
+        print("There are no local maxima.")
 
-    
-    
+
 if __name__ == "__main__":
     length = int(input("Please enter the length of your array: "))
     array = [0] * length
 
     for i in range(length):
-        array[i] = int(input(f"{i + 1}. Enter your number: "))     
-          
+        array[i] = int(input(f"{i + 1}. Enter your number: "))
+
+    find_Local_Maxima(length, array)
