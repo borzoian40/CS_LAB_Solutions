27377e0470a27919b1185bda6cdcc4f5d22de2c2
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-12-02 18:22:02 +0100

    Update Ex_7.1.4.py

--- LAB7/Ex_7.1.4.py
+++ LAB7/Ex_7.1.4.py
@@ -1,16 +1,21 @@
 def find_Local_Maxima(length, array):
     # An array to store the indices of the local maxima values.
     maxima = []
-
+    
+    #Check if the value located in the first index is greater than the adjacent.
     if (array[0] > array[1]):
         maxima.append(0)
+    
+    #Iterate through the array to check for the local maxima
 
     for i in range(1, length - 1):
         if (array[i - 1] < array[i] > array[i + 1]):
             maxima.append(i)
-
+    #If the last element is greater than the previous element.
     if (array[-1] > array[-2]):
         maxima.append(0)
+        
+    #Check whether the array is null or not.    
     if (len(maxima) > 0):
         print("Indices where the local maxima is located: ")
         print(*maxima)
